b72b2788623dd402f01f84655669fbd872fd3682
Pavan Hassamal <phass001@gmail.com>
2018-08-22 12:31:15 -0700

    loops example

---
+++ loopsProblems.js
@@ -0,0 +1,12 @@
+console.log("NUMBERS BETWEEN -10 AND 19");
+
+for(var i = -10; i < 20; i++){
+	console.log(i);
+}
+
+console.log("EVEN NUMBERS BETWEEN 10 AND 40")
+
+for(var i = 10; i <= 40; i+=2){
+	console.log(i);
+}
+
